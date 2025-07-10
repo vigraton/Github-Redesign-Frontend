df489b18b375776cba9ca8da1e2fc3fc64437e56
vigraton <viviangraton@gmail.com>
2025-07-09 22:27:10 -0300

    add mocked data to repo card

--- src/app/components/RepositoryCard/card.tsx
+++ src/app/components/RepositoryCard/card.tsx
@@ -1,23 +1,27 @@
 import { FaRegStar } from "react-icons/fa6";
 import { IoMdArrowDropdown } from "react-icons/io";
 
-export default function RepositoryCard() {
+type Project = {
+  title: string,
+  language: string
+};
+
+export default function RepositoryCard({ title, language }: Project) {
   return (
-    <div className="flex flex-col border-2 border-[#3d444d] bg-[#212830] rounded-2xl h-50 w-80">
+    <div className="flex flex-col translate-y-44 border-2 border-[#3d444d] bg-[#212830] rounded-2xl h-50 w-80">
       <h2 className="h-fit p-2 text-[#418BE6] font-semibold text-2xl mt-5 ml-4">
-        Projeto
+        {title}
       </h2>
       <div className="absolute flex flex-row items-center place-items-start p-2 ml-4 mt-16 h-fit">
         <span className="flex h-3 w-3 rounded-lg bg-[#F1E05A]"></span>
-        <p className="text-[#9198A1] ml-1">JavaScript</p>
+        <p className="text-[#9198A1] ml-1">{language}</p>
         <p className="text-[#9198A1] ml-3 text-nowrap">
           Updated 37 minutes ago
         </p>
       </div>
       <button className="absolute flex flex-row place-items-start items-center gap-20 ml-4 mt-30 border-2 border-[#3d444d] bg-[#212830] rounded-lg h-10 w-30">
-        <FaRegStar className="text-[#9198A1] w-5 h-5 gap-2 flex" />
-        {/* <p className="text-[#9198A1]">Star</p> */}
-        <IoMdArrowDropdown className="text-[#9198A1] w-5 h-5 gap-2 flex" />
+        <FaRegStar className="text-[#9198A1] w-5 h-5 gap-2 flex translate-x-2" />
+        <IoMdArrowDropdown className="text-[#9198A1] w-5 h-5 gap-2 flex -translate-x-2" />
       </button>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,32 @@ import RepositoryCard from "../app/components/RepositoryCard/card";
 import Header from "../app/components/Header/header";
 
 export default function Home() {
+  const repos = [
+    {
+      id: 1,
+      title: "GitHub Redesign",
+      language: "TypeScript"
+    },
+    {
+      id: 2,
+      title: "Mimimi",
+      language: "Go"
+    },
+    {
+      id: 3,
+      title: "Momomo",
+      language: "Java"
+    },
+  ];
+
   return (
-    <div className="flex flex-col">
-      {/* <RepositoryCard /> */}
+    <div className="flex flex-col items-center min-h-screen">
       <Header />
+      <div className="flex flex-row gap-4">
+        {repos.map((repo) => (
+          <RepositoryCard key={repo.id} {...repo} />
+        ))}
+      </div>
     </div>
   );
 }
