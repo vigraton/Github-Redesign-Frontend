cf115de169e652b45de65b91cbee9a323290d1c7
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2026-06-11 23:54:36 +0000

    wip: adjusting header

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -6,6 +6,43 @@ import { usePathname } from "next/navigation";
 
 export default function Header() {
   const pathname = usePathname();
+  const nav_items = [
+    {
+      id: 1,
+      nav: "Overview",
+      ref: "/overview",
+      icon: "/book_icon.svg",
+      isActive: pathname === "/overview",
+    },
+    {
+      id: 2,
+      nav: "Repositories",
+      ref: "/repositories",
+      icon: "/repository_icon.svg",
+      isActive: pathname === "/repositories",
+    },
+    {
+      id: 3,
+      nav: "Projects",
+      ref: "/projects",
+      icon: "/projects_icon.svg",
+      isActive: pathname === "/projects",
+    },
+    {
+      id: 4,
+      nav: "Packages",
+      ref: "/packages",
+      icon: "/packages_icon.svg",
+      isActive: pathname === "/packages",
+    },
+    {
+      id: 5,
+      nav: "Stars",
+      ref: "/stars",
+      icon: "/star.svg",
+      isActive: pathname === "/stars",
+    },
+  ];
 
   return (
     <div className="bg-[#151b23] w-full h-28 fixed top-0 z-10">
@@ -24,41 +61,46 @@ export default function Header() {
       </div>
 
       <nav className="flex flex-row gap-2 pt-6 pb-2 pl-2">
-        <Link
+        {nav_items.map((item) => (
+          <div key={item.id} className="flex flex-row items-center gap-4 text-muted">
+            <img src={item.icon}>{item.icon}</img>
+            <Link href={item.ref} className={`${item.isActive ? "border border-b-[#8A38F5]" : ""} cursor pointer`}>
+              {item.nav}
+            </Link>
+          </div>
+        ))}
+
+        {/* <Link
           href="#"
           className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730] [&.active]:border-b-2 [&.active]:border-orange-400 [&.active]:bottom-0 ${
             pathname === "#" ? "active" : ""
-          }`}
-        >
+          }`}>
           Overview
         </Link>
         <Link
           href="#"
           className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730]`}
-          data-active="true"
-        >
+          data-active="true">
           Repositories
         </Link>
         <Link
           href="/repositories"
           className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730] ${
             pathname === "/projects" ? "active" : ""
-          }`}
-        >
+          }`}>
           Projects
         </Link>
         <Link
           href="#"
-          className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730]`}
-        >
+          className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730]`}>
           Packages
         </Link>
         <Link
           href="#"
-          className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730]`}
-        >
+          className={`text-white text-[14px] rounded-[6px] px-4 py-1 hover:bg-[#212730]`}>
           Stars
-        </Link>
+        </Link> */}
+
       </nav>
     </div>
   );
--- src/app/components/RepositoryCard/card.tsx
+++ src/app/components/RepositoryCard/card.tsx
@@ -17,10 +17,6 @@ export default function RepositoryCard({
   const [starred, setStarred] = useState(false);
   const isPrivate = useState(false);
 
-  const handleStarClick = () => {
-    setStarred(!starred);
-  };
-
   return (
     <div className="flex flex-col relative border-2 border-[#3d444d] bg-[#212830] rounded-2xl p-4 h-45 w-75">
       <div className="space-y-3">
@@ -37,11 +33,9 @@ export default function RepositoryCard({
         </div>
 
         {
-          <div>
-            <p className="text-[13px] font-normal text-[#a4a4a4] truncate">
-              {description}
-            </p>
-          </div>
+          <p className="text-[13px] font-normal text-[#a4a4a4] truncate">
+            {description}
+          </p>
         }
 
         <div className="flex flex-row items-center text-sm text-[#9198A1] justify-between">
@@ -54,18 +48,21 @@ export default function RepositoryCard({
       </div>
 
       <button
-        onClick={handleStarClick}
+        onClick={() => {
+          setStarred(!starred);
+        }}
         className={`${starred ? "bg-[#D29922]/20 border-[#D29922] text-[#D29922]" : "bg-[#212830] border-[#3d444d] text-[#9198A1]"} cursor-pointer absolute flex bottom-4 items-center justify-between border-1 rounded-lg h-10 w-[55px]`}>
         {starred ? (
           <FaStar className="text-[#FFCC00] w-4.5 h-4.5 ml-2" />
         ) : (
           <FaRegStar className="text-[#9198A1] w-4.5 h-4.5 ml-2" />
         )}
-        <span className="mr-2">{(stargazers_count ?? 0) + (starred ? 1 : 0)}</span>
+        <span className="mr-2">
+          {(stargazers_count ?? 0) + (starred ? 1 : 0)}
+        </span>
 
         {/* Cagaio, acho que finalmente entendi ??. Se stargazers_count vir como null ou undefined da API, 
         então vai ficar como 0. Se estiver marcado então é contado + 1 */}
-
       </button>
     </div>
   );
