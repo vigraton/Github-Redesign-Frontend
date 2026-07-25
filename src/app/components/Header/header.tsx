75289aab9bb917d7878c186d023f121b9eba5e6b
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2026-07-25 23:51:45 +0000

    feat: try to deploy

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useUserContext } from "@/app/context/UserContext";
