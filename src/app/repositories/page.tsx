7fb28bb989beac2dfcc2f1c5e0b1a639f7286117
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2026-07-29 01:10:19 +0000

    feat: try to deploy

--- src/app/repositories/page.tsx
+++ src/app/repositories/page.tsx
@@ -15,7 +15,7 @@ export default function RepositoriesPage({ username }: RepoPageProps) {
 
   useEffect(() => {
     if (username) fetchUserRepos(username);
-  }, [username, repos]);
+  }, []);
 
   return (
     <main className="pt-40 bg-[#212830] overflow-x-hidden flex flex-col justify-between gap-20">
